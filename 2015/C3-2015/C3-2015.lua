4defae0f7bd0f9edd4deb6a50c0eeeeddf9ac30d
Sean <sshuy@protonmail.ch>
2023-09-07 01:07:20 +0100

    Uploaded prep for C3.
    Gonna use a .txt file instead to hold the puzzle input.

---
+++ 2015/C3-2015/C3-2015.lua
@@ -0,0 +1,11 @@
+-- Day 3: Perfectly Spherical Houses in a Vacuum
+-- https://adventofcode.com/2015/day/3
+
+local f = "puzzle_input.txt"
+local lines = {}
+for line in io.lines(f) do
+    lines[#lines+1] = line
+end
+
+for _,v in pairs(lines) do
+end
